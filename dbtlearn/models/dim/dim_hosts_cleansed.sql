db536810d42034aa5b7c803c183fe6db25f801e0
Mehran Moazeni <moazeni.mehran@gmail.com>
2024-10-15 14:37:15 +0200

    model 3 for cleansed added

---
+++ dbtlearn/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,16 @@
+WITH src_hosts AS (
+    SELECT 
+    * 
+    FROM {{ref("src_hosts")}}
+)
+SELECT
+    host_id,
+    NVL(
+        host_name,
+        'Anonymous'
+    ) AS host_name,
+    is_superhost,
+    host_create,
+    host_updated
+FROM
+    src_hosts
